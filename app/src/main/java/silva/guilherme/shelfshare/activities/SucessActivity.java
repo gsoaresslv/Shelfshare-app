3aa9d6c32ee451925bcb052d0061271759431d6f
20221tiimi0149 <guilhermesoaresvixbr@gmail.com>
2025-01-31 10:36:54 -0300

    falta muita coisa

---
+++ app/src/main/java/silva/guilherme/shelfshare/activities/SucessActivity.java
@@ -0,0 +1,34 @@
+package silva.guilherme.shelfshare.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import silva.guilherme.shelfshare.R;
+
+public class SucessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sucess);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Button btn_sucess = findViewById(R.id.btn_sucess);
+        btn_sucess.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        });
+    }
+}
