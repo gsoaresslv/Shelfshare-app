0e92a8eae865b64cb0837baeeea6363c3fe79b63
gsoaresslv <guilhermesoaresvixbr@gmail.com>
2025-02-04 09:41:27 -0300

    atualizações

---
+++ app/src/main/java/silva/guilherme/shelfshare/activities/BookActivity.java
@@ -0,0 +1,34 @@
+package silva.guilherme.shelfshare.activities;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import silva.guilherme.shelfshare.R;
+
+public class BookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_book);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Toolbar tb_book = findViewById(R.id.tb_book);
+        setSupportActionBar(tb_book);
+        ActionBar book_actionBar = getSupportActionBar();
+        book_actionBar.setDisplayHomeAsUpEnabled(true);
+
+    }
+}
